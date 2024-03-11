ab56030c01cd37437b98283312cccf26a137329f
railanbaigazy <baigazy.railan@gmail.com>
2024-03-11 21:40:21 +0500

    parentnode + unittest

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,16 +9,17 @@ class HTMLNode:
         raise NotImplementedError
     
     def props_to_html(self):
-        html = ""
+        props_html = ""
         if self.props:
             for key, value in self.props.items():
-                html += " " + key + f'="{value}"'
+                props_html += " " + key + f'="{value}"'
         
-        return html
+        return props_html
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
     
+
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
@@ -35,3 +36,22 @@ class LeafNode(HTMLNode):
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children):
+        super().__init__(tag, None, children, None)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("No required tag was provided")
+        if not self.children:
+            raise ValueError("No required children provided")
+        
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+
+        return (
+            f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+        )
+        
--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
 from textnode import TextNode
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, ParentNode, LeafNode
 
 
 def main():
     txtnod = TextNode("Text check", "bold", "https://github.com")
     htmlnod = HTMLNode("a", "Text check", None, {"href": "https://www.google.com"})
+    parentnod = ParentNode(
+    "p",
+    [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+    ],
+)
+
     print(txtnod)
     print(htmlnod)
+    print(parentnod.to_html())
 
 
 main()
