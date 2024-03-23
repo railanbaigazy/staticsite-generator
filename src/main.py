b73af8fe2c0bf0674c2589a33e7e713a36fca7c4
railanbaigazy <baigazy.railan@gmail.com>
2024-03-23 16:36:20 +0500

    copy static main.py

--- src/main.py
+++ src/main.py
@@ -1,23 +1,37 @@
-from textnode import TextNode
-from htmlnode import HTMLNode, ParentNode, LeafNode
+import os
+import shutil
+import logging
 
+def copy_directory_contents(source_dir, dest_dir):
+    if not os.path.exists(source_dir):
+        logging.error(f"Source directory '{source_dir}' does not exist.")
+        return
+
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    
+    for item in os.listdir(source_dir):
+        source_item = os.path.join(source_dir, item)
+        dest_item = os.path.join(dest_dir, item)
+        
+        if os.path.isdir(source_item):
+            copy_directory_contents(source_item, dest_item)
+        elif os.path.isfile(source_item):
+            shutil.copy(source_item, dest_item)
+            logging.info(f"Copied file: {source_item} -> {dest_item}")
+        else:
+            logging.warning(f"Ignoring item: {source_item}")
 
 def main():
-    txtnod = TextNode("Text check", "bold", "https://github.com")
-    htmlnod = HTMLNode("a", "Text check", None, {"href": "https://www.google.com"})
-    parentnod = ParentNode(
-    "p",
-    [
-        LeafNode("b", "Bold text"),
-        LeafNode(None, "Normal text"),
-        LeafNode("i", "italic text"),
-        LeafNode(None, "Normal text"),
-    ],
-)
+    source_dir = "static"
+    dest_dir = "public"
+    
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+        logging.info(f"Deleted contents of {dest_dir}")
+    
+    copy_directory_contents(source_dir, dest_dir)
 
-    print(txtnod)
-    print(htmlnod)
-    print(parentnod.to_html())
-
-
-main()
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
